01e497d35ca4485f0098b521838207b69034c9b3
MGallo-Code <mgallo.code@gmail.com>
2025-01-02 17:39:27 -0500

    Add Drone class for chord playback with specified octaves

---
+++ Drone.py
@@ -0,0 +1,9 @@
+from Note import Note
+from sound_utils import play_chord
+
+class Drone:
+    def __init__(self, note="C", drone_octaves=[-6, -4, -2, 0, 2, 4], duration=5, delay=0):
+        self.notes = [Note(note=note, octave=i, duration=duration, delay=delay) for i in drone_octaves]
+
+    def play(self):
+        play_chord(self.notes)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from Drone import Drone
 from Note import Note
 from sound_utils import play_chord
 from music_utils import (
@@ -7,18 +8,8 @@ from music_utils import (
 )
 
 if __name__ == "__main__":
-    drone_duration = 5
-    drone_notes = [
-        Note("C", octave=-6, duration=drone_duration),
-        Note("C", octave=-4, duration=drone_duration),
-        Note("C", octave=-2, duration=drone_duration),
-        Note("C", octave=0, duration=drone_duration),
-        Note("C", octave=2, duration=drone_duration),
-        Note("C", octave=4, duration=drone_duration),
-    ]
-
     # Play drone
-    play_chord(drone_notes)
+    Drone("C").play()
 
     # Play a major scale
     # play_scale(scale_intervals=MAJOR_SCALE_INTERVALS, start_note="F", octave=4)
