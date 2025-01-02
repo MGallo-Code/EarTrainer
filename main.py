bf49929f73577cfa41f2efcb5c29e15121c0f991
MGallo-Code <mgallo.code@gmail.com>
2025-01-02 17:25:46 -0500

    Add drone playback feature using play_chord with multiple octaves

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from Note import Note
+from sound_utils import play_chord
 from music_utils import (
     play_scale,
     MAJOR_SCALE_INTERVALS,
     MINOR_SCALE_INTERVALS
 )
-from sound_utils import play_chord
 
 if __name__ == "__main__":
-    # Play a single note
-    Note("C", 4).play()
+    drone_duration = 5
+    drone_notes = [
+        Note("C", octave=-6, duration=drone_duration),
+        Note("C", octave=-4, duration=drone_duration),
+        Note("C", octave=-2, duration=drone_duration),
+        Note("C", octave=0, duration=drone_duration),
+        Note("C", octave=2, duration=drone_duration),
+        Note("C", octave=4, duration=drone_duration),
+    ]
 
-    # Play a chord
-    play_chord([Note("C", 4), Note("E", 4), Note("G", 4)])
+    # Play drone
+    play_chord(drone_notes)
 
     # Play a major scale
-    play_scale(scale_intervals=MAJOR_SCALE_INTERVALS, start_note="F", octave=4)
-    play_scale(scale_intervals=MINOR_SCALE_INTERVALS, start_note="F", octave=4)
+    # play_scale(scale_intervals=MAJOR_SCALE_INTERVALS, start_note="F", octave=4)
+    # play_scale(scale_intervals=MINOR_SCALE_INTERVALS, start_note="F", octave=4)
