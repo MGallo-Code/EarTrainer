e6967fa6f5c56fc4ed88d37b30f0113ed066ba2e
MGallo-Code <mgallo.code@gmail.com>
2025-01-02 20:19:15 -0500

    Move main testing script to test.py and remove main.py

---
+++ test.py
@@ -0,0 +1,24 @@
+# test.py
+# Test script to test music/sound.
+
+from Drone import Drone
+from Note import Note
+from sound_utils import play_chord
+from music_utils import (
+    play_scale,
+    MAJOR_SCALE_INTERVALS,
+    MINOR_SCALE_INTERVALS
+)
+
+if __name__ == "__main__":
+    # Testing Note Volume
+    Note("C", volume=0.1).play()
+    Note("C", volume=0.5).play()
+    Note("C", volume=1).play()
+
+    # Play drone
+    Drone("C", volume=1).play()
+
+    # Play a major scale
+    # play_scale(scale_intervals=MAJOR_SCALE_INTERVALS, start_note="F", octave=4)
+    # play_scale(scale_intervals=MINOR_SCALE_INTERVALS, start_note="F", octave=4)
